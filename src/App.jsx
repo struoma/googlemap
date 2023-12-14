db1617840993f2c8be17d487e6bdc658e2382542
struoma <stephen.afekhuai@gmail.com>
2023-12-14 05:36:41 +0100

    my commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+function App() {
+  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+  return (
+    <>
+      {/*Input your key for map to show*/}
+      <div style={{ height: "100vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        </GoogleMapReact>
+      </div>
+    </>
+  );
+}
+
+export default App;
